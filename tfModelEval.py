dae4e8b2e6a8451340c8238b66626c14ec2bdcfa
mgrecu35 <mirgrecu@gmail.com>
2023-01-03 08:26:29 -0500

    update

--- tfModelEval.py
+++ tfModelEval.py
@@ -5,7 +5,8 @@ neigh = KNeighborsRegressor(n_neighbors=30,weights='distance')
 from sklearn.model_selection import train_test_split
 
 with Dataset("radiometer_obs_2020-08-11_04:10:00_subset.nc") as fh:
-    yobsL=fh["tb"][:,:]
+    yobsL=fh["tb2"][:,:]
+    yobs2L=fh["tb"][:,:]
 with Dataset("active_obs_2020-08-11_04:10:00_subset.nc") as fh:
     x2D=fh["iwc"][:,15:55]
     zku=fh["zKu"][:]
@@ -21,7 +22,7 @@ import matplotlib
 import pickle
 zku_test=zku
 import tensorflow as tf
-d=pickle.load(open("scalersAndPCA.pklz","rb"))
+d=pickle.load(open("dfnscalersAndPCA.pklz","rb"))
 pca=d["PCA"]
 scalerPCA=d["scalerPCA"]
 scalerX=d["scalerX"]
@@ -30,7 +31,7 @@ scalerIWC=d["scalerIWC"]
 zku_test[zku_test<8]=0
 xnn_test=np.concatenate((zku_test[:,15:55],X_test[:,:]),axis=-1)
 xnn_test=scalerX.transform(xnn_test)
-tfmodel=tf.keras.models.load_model('radar_radiom_tf_model.h5')
+tfmodel=tf.keras.models.load_model('dfnradar_radiom_tf_model.h5')
 ynn_=neigh.predict(X_test)
 
 y_=tfmodel.predict(xnn_test)
@@ -46,7 +47,7 @@ ynnf_=ynn_.flatten()
 iwcf=iwc_test.flatten()                              # flatten the test IWC
 plt.figure()
 ax1=plt.subplot(121)
-plt.hist2d(yf_,iwcf,bins=np.arange(100)*0.01,\
+plt.hist2d(iwcf,yf_,bins=np.arange(100)*0.01,\
            norm=matplotlib.colors.LogNorm(),cmap='jet')
 x1=np.arange(101)*0.01
 plt.plot(x1,x1)
@@ -59,7 +60,7 @@ cb1.ax.set_xlabel('Counts')
 
 if 1==1:
     ax2=plt.subplot(122)
-    plt.hist2d(ynnf_,iwcf,bins=np.arange(100)*0.01,\
+    plt.hist2d(iwcf,ynnf_,bins=np.arange(100)*0.01,\
                 norm=matplotlib.colors.LogNorm(),cmap='jet')
     plt.plot(x1,x1)
     ax2.set_aspect('equal')
@@ -68,5 +69,5 @@ if 1==1:
     cb2=plt.colorbar(orientation='horizontal')
     cb2.ax.set_xlabel('Counts')
 
-plt.savefig('retrievals_PCAsfromRet_hist2d.png')
+plt.savefig('retrievals_PCAsfromRet_noise_hist2d.png')
 
